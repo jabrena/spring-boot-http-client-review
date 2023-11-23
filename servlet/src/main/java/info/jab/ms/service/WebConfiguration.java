dea76de4772342311b4f4da27bb7ccc454d53fbb
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-11-23 12:51:49 +0100

    Upgrading README (#8)

--- servlet/src/main/java/info/jab/ms/service/WebConfiguration.java
+++ servlet/src/main/java/info/jab/ms/service/WebConfiguration.java
@@ -1,6 +1,5 @@
 package info.jab.ms.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
